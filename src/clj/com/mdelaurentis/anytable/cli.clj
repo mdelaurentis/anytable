06573894cc2b907c7a185fe9936388c9e655974b
Michael Delaurentis <mdelaurentis@macbook-2.local>
2009-10-02 19:10:23 -0400

    Add defcmd macro and improved help for commands

--- src/clj/com/mdelaurentis/anytable/cli.clj
+++ src/clj/com/mdelaurentis/anytable/cli.clj
@@ -2,6 +2,7 @@
 
 (ns com.mdelaurentis.anytable.cli
   (:import [java.io File]
+           [java.util.regex Pattern]
            [java.net URI MalformedURLException URL])
   (:require [clojure.contrib.duck-streams :as streams])
   (:use [clojure.contrib sql command-line]
@@ -11,11 +12,22 @@
 (defmulti main (fn [cmd & args]
                  cmd))
 
-(defmethod main :cp [cmd & args]
+(def commands (ref {}))
+
+(defmacro defcmd [cmd desc cmdspec & body]
+  (dosync
+   (alter commands assoc cmd {:desc desc :cmdspec cmdspec})
+   `(defmethod main ~cmd [_# & args#]
+      (with-command-line args# ~desc ~cmdspec ~@body))))
+
+(defcmd :cp
+    "Copy a table from one place to another."
+    [args]
   (when (not (= 2 (count args)))
     (throw (Exception. "Usage: anytable cp <in-spec> <out-spec>")))
   (let [[in out] args]
     (copy (anytable-spec in) (anytable-spec out))))
+
 
 (defn do-cat [out-spec readers specs]
   (if specs
@@ -31,71 +43,73 @@
     (anytable-spec spec)
     (table-types ::tab)))
 
-(defmethod main :cat [cmd & args]
-  (with-command-line 
-   args
-   "cat - Concatenate some tables together.
-Usage: anytable cat [options] <in-spec> [<in-spec>...]"
+(defcmd :cat
+   "Concatenate some tables together.
+Usage: anytable cat [options] <in-spec> [<in-spec>...]"  
    [[out o "Write output to here."]
     specs]
    (let [dest (if out
                 (anytable-spec out)
                 (table-types ::tab))]
-     (do-cat dest [] (map anytable-spec specs)))))
+     (do-cat dest [] (map anytable-spec specs))))
 
-(defmethod main :cut [cmd & args]
-  (with-command-line
-      args
-      "cut - Select some columns from the input table.
-Usage: anytable cut [options] field [field...]"  
+(defcmd :cut
+      "Select some columns from the input table.
+Usage: anytable cut [options] field [field...]"
       [[in i "Read input from here."]
        [out o "Write output to here."]  
-       cols]
+       cols] 
+    (println "Cutting " cols "from" in)
     (with-reader [r (spec-or-default in)]
       (with-writer [w (assoc (spec-or-default out)
                         :headers cols)]
-        (reduce write-row w (row-seq r))))))
+        (reduce write-record w (record-seq r)))))
 
-(defmethod main :rename [cmd & args]
-  (with-command-line
-      args
-      "rename - Rename some columns.
+(defcmd :rename
+      "Rename some columns.
 Usage: anytable rename [options] in1 out1 [in2 out2 ...]
 
 Each pair of in* out* values will cause anytable to rename the column
-identified by in* to out*."  
+identified by in* to out*." 
       [[in i "Read input from here."]
        [out o "Write output to here."]  fields]
-    (with-reader [r (spec-or-default in)]
-      (let [pairs        (partition 2 fields)
-            replacements (zipmap (map first pairs) (map second pairs))
-            cols         (replace replacements (headers r))]
-        (with-writer [w (assoc (spec-or-default out)
-                          :headers cols)]
-          (reduce write-row w (row-seq r)))))))
-
-(defmethod main :filter [cmd & args]
-  (with-command-line
-      args
-      "filter - Select rows from an input table based on some criteria."  
-      [[in i "Read input from here."]
-       [out o "Write output to here."]  
-       crit]
-    (let [crit (eval (read-string (first crit)))]
-      (when-not (fn? crit)
-        (throw (Exception. "criteria should evaluate to a function that takes a record")))
+      (println "Renaming stuff from" in "to" out ":" fields)
       (with-reader [r (spec-or-default in)]
-        (with-writer [w (assoc (spec-or-default out)
-                          :headers (headers r))]
-          (doseq [rec (record-seq r)]
-            (when (crit rec)
-              (write-record w rec))))))))
+        (let [pairs        (partition 2 fields)
+              replacements (zipmap (map first pairs) (map second pairs))
+              cols         (replace replacements (headers r))]
+          (with-writer [w (assoc (spec-or-default out)
+                            :headers cols)]
+            (reduce write-row w (row-seq r))))))
+
+(defcmd :filter 
+  "Select rows from an input table based on some criteria."  
+  [[in i "Read input from here."]
+   [out o "Write output to here."]  
+   crit]
+  (let [crit (eval (read-string (first crit)))]
+    (when-not (fn? crit)
+      (throw (Exception. "criteria should evaluate to a function that takes a record")))
+    (with-reader [r (spec-or-default in)]
+      (with-writer [w (assoc (spec-or-default out)
+                        :headers (headers r))]
+        (doseq [rec (record-seq r)]
+          (when (crit rec)
+            (write-record w rec)))))))
+
+(defn first-sentence [string]
+  (second (re-matches (Pattern/compile "(.+?\\.).*" Pattern/DOTALL) string)))
 
 (defmethod main :help 
   ([help]
      (println "Usage: anytable <command> [options]")
-     (doseq [method (keys (methods main))]
-       (println " " (.substring (str method) 1))))
+     (newline)
+     (println "Commands:")
+     (doseq [cmd (keys (methods main))]
+       (println " " (.substring (str cmd) 1) "-" 
+                (first-sentence (:desc (commands cmd) ""))))
+     (newline)
+     (println "Try \"anytable help <cmd>\" to get detailed help for a command."))
 
   ([help & args]
      (let [arg  (first args)
@@ -109,7 +123,8 @@ identified by in* to out*."
              (println " " (.getName k) "-" (:doc ^v))))
          
          ((methods main) cmd)
-         (println "Give help for 'anytable" arg "'")
+         (print-help (:desc (commands cmd))
+                     (make-map [] (:cmdspec (commands cmd))))
          
          (table-types type)
          (let [default (table-types type)]
